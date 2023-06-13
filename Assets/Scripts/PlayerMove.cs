13c757459809477b4861a79a78af3cff8d4ad4c8
thuan <minhthuanst01@gmail.com>
2023-06-13 22:55:51 +0700

    Add screen Lobby and Map

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,108 +1,108 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float dashDistance = 1f;
-    public float dashDuration = 0.2f;
-    public float dashCooldown = 0.2f;
 
-    private bool isDashing = false;
-    private bool canDash = true;
+    public int moveX = 1;
+    public int moveY = 1;
 
-    //[SerializeField] private GameObject player;
-    private Rigidbody2D rb;
-    private Vector3 dashDirection;
-    // Start is called before the first frame update
-    void Start()
+    //private bool isGrounded =false;
+    [SerializeField] Transform checkTop;
+    [SerializeField] Transform checkLeft;
+    [SerializeField] Transform checkRight;
+    [SerializeField] Transform checkDown;
+
+    [SerializeField] LayerMask brickLayer;
+    [SerializeField]
+    private bool isTop;
+    [SerializeField]
+    private bool isLeft;
+    [SerializeField]
+    private bool isRight;
+    [SerializeField]
+    private bool isDown;
+
+    private void Update()
     {
-        rb = GetComponent<Rigidbody2D>();
+        CheckTop();
+        CheckLeft();
+        CheckRight();
+        CheckDown();
+        if (Input.GetKeyDown(KeyCode.LeftArrow)&& !isLeft)
+        {
+            transform.Translate(new Vector3(-moveX, 0, 0));
+
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && !isRight)
+        {
+
+            transform.Translate(new Vector3(moveX, 0, 0));
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) && !isTop)
+        {
+
+            transform.Translate(new Vector3(0, moveY, 0));
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && !isDown)
+        {
+
+            transform.Translate(new Vector3(0, -moveY, 0));
+        }
+      
+    }
+    void CheckTop()
+    {
+        isTop = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(checkTop.position, 0.2f, brickLayer);
+        if (colliders.Length > 0)
+            isTop = true;
+    }
+    void CheckLeft()
+    {
+        isLeft = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(checkLeft.position, 0.2f, brickLayer);
+        if (colliders.Length > 0)
+            isLeft = true;
+    }
+    void CheckRight()
+    {
+        isRight = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(checkRight.position, 0.2f, brickLayer);
+        if (colliders.Length > 0)
+            isRight = true;
+    }
+    void CheckDown()
+    {
+        isDown = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(checkDown.position, 0.2f, brickLayer);
+        if (colliders.Length > 0)
+            isDown = true;
+    }
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.collider.tag == "Player")
+    //    {
+    //        Debug.Log("true");
+    //        SceneManager.LoadScene("New");
+    //    }
+    //}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("SingleMode"))
+        {
+            Debug.Log("Single");
+            
+            SceneManager.LoadScene("PlayMode");
+        }
+        else if (collision.CompareTag("CoopMode"))
+        {
+            Debug.Log("Coop");
+            
+           SceneManager.LoadScene("Map");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            if (canDash && !isDashing)
-            {
-                // Store the direction of dash based on player's input
-                dashDirection = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f).normalized;
-
-                // Start dashing
-                StartCoroutine(Moving());
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            if (canDash && !isDashing)
-            {
-                // Store the direction of dash based on player's input
-                dashDirection = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f).normalized;
-
-                // Start dashing
-                StartCoroutine(Moving());
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            if (canDash && !isDashing)
-            {
-                // Store the direction of dash based on player's input
-                dashDirection = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f).normalized;
-
-                // Start dashing
-                StartCoroutine(Moving());
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            if (canDash && !isDashing)
-            {
-                // Store the direction of dash based on player's input
-                dashDirection = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f).normalized;
-
-                // Start dashing
-                StartCoroutine(Moving());
-            }
-        }
-
-    }
-    
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.collider.CompareTag("Brick"))
-        {
-            dashDistance = 0f;
-        }
-    }
-
-    private IEnumerator Moving()
-    {
-        isDashing = true;
-        canDash = false;
-        // Disable player's movement during the dash
-        var originalPosition = transform.position;
-        var targetPosition = originalPosition + dashDirection * dashDistance;
-        var elapsedTime = 0f;
-
-        while (elapsedTime < dashDuration)
-        {
-            // Lerp between the original position and target position based on time
-            transform.position = Vector3.Lerp(originalPosition, targetPosition, elapsedTime / dashDuration);
-
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-
-        // Snap to the target position
-        transform.position = targetPosition;
-
-        // Wait for dash cooldown
-        yield return new WaitForSeconds(dashCooldown);
-        canDash = true;
-        isDashing = false;
-
-    }
 }
