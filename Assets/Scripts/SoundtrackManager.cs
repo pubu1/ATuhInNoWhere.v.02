6f3d1ad597127e87a8a9aacc2531a9662cf85559
yelan88 <106862627+Yelan88@users.noreply.github.com>
2023-06-15 18:17:36 +0700

    push setting volume soundtrack

--- Assets/Scripts/SoundtrackManager.cs
+++ Assets/Scripts/SoundtrackManager.cs
@@ -1,7 +1,8 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SoundtrackManager : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class SoundtrackManager : MonoBehaviour
     
     [SerializeField]
     private AudioSource[] audio;
+
+    [SerializeField]
+    private Slider volumeSlider;
 
     private void Awake()
     {
@@ -27,6 +31,8 @@ public class SoundtrackManager : MonoBehaviour
     {
         audio[0].enabled = true;
         audio[1].enabled = false;
+
+        volumeSlider.onValueChanged.AddListener(OnVolumeSliderValueChanged);
     }
 
     void Update(){
@@ -36,6 +42,16 @@ public class SoundtrackManager : MonoBehaviour
         } else {
             audio[0].enabled = false;
             audio[1].enabled = true;
+        }
+    }
+
+    // Hàm xử lý sự kiện thay đổi giá trị của Slider
+    void OnVolumeSliderValueChanged(float value)
+    {
+        // Cập nhật âm lượng dựa trên giá trị của Slider
+        foreach (AudioSource source in audio)
+        {
+            source.volume = value;
         }
     }
 }
