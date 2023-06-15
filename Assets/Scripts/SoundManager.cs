d369b62ae034753b65e5461989fefdac2ef12add
yelan88 <106862627+Yelan88@users.noreply.github.com>
2023-06-15 17:38:09 +0700

    fix UI

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -8,26 +8,5 @@ public class SoundManager : MonoBehaviour
     private AudioSource audioSource;
     private Slider volumeSlider;
 
-    private void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-        volumeSlider = GetComponentInChildren<Slider>();
-    }
-
-    public void ToggleSound()
-    {
-        if (audioSource.isPlaying)
-        {
-            audioSource.Stop();
-        }
-        else
-        {
-            audioSource.Play();
-        }
-    }
-
-    public void SetVolume(float value)
-    {
-        audioSource.volume = value;
-    }
+    
 }
