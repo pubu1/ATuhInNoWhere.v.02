191d338471f94c70515fccc80785e5cd11f5ced9
Phuong Uyen <lhpuyen.work@gmail.com>
2023-06-17 17:54:21 +0700

    update loading scene

--- Assets/Scripts/Lobby/AsyncManager.cs
+++ Assets/Scripts/Lobby/AsyncManager.cs
@@ -4,64 +4,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class AsyncManager : MonoBehaviourPunCallbacks
 {
     private bool connectedToMaster = false;
 
-    [Header("Menu Screens")]
     [SerializeField]
-    private GameObject loadingScene;
+    private TMP_Text loadingBtn;
 
-    [SerializeField]
-    private GameObject mainMenu;
-
-    [Header("Slider")]
-    [SerializeField]
-    private Slider loadingSlider;
-
-    private bool isLoading = false;
-
-    void Start()
+    public void OnClickConnect()
     {
+        loadingBtn.text = "Connecting...";
         PhotonNetwork.ConnectUsingSettings();
     }
 
     public override void OnConnectedToMaster()
-    {
+    {   
         connectedToMaster = true;
         PhotonNetwork.JoinLobby();
     }
 
-    public void OnButtonClick()
+    public override void OnJoinedLobby()
     {
-        Debug.Log("start");
         if (connectedToMaster)
         {
-            LoadLevelBtn("Lobby");
+            SceneManager.LoadScene("Lobby");
         }
     }
 
-    public void LoadLevelBtn(string levelToLoad)
-    {
-        if (!isLoading)
-        {
-            isLoading = true;
-            mainMenu.SetActive(false);
-            loadingScene.SetActive(true);
-            StartCoroutine(LoadLevelAsync(levelToLoad));
-        }
-    }
-
-    IEnumerator LoadLevelAsync(string levelToLoad)
-    {
-        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(levelToLoad);
-        while (!loadOperation.isDone)
-        {
-            float progress = Mathf.Clamp01(loadOperation.progress / .9f);
-            loadingSlider.value = progress;
-            yield return new WaitForSeconds(10.0f);
-        }
-        isLoading = false;
-    }
 }
--- Assets/Scripts/Lobby/LobbyManager.cs
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -3,10 +3,19 @@ using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
+using System.IO;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
+    [Header("Room Name")]
     [SerializeField] private TMP_Text roomNameText;
+
+    /*[Header("Map Name")]
+    [SerializeField] private List<string> mapList = new List<string>();
+    private string mapFolderPath = "CustomMap/"; // Replace with the actual folder path
+    private string[] mapFiles = Directory.GetFiles(mapFolderPath, "*.txt");*/
+
     [SerializeField] private TMP_Text mapNameText;
 
     private void Start()
@@ -15,6 +24,8 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         roomNameText.text = PhotonNetwork.CurrentRoom.Name;
         mapNameText.text = "Default Map";
     }
+
+
 
     public void SelectMap(string mapName)
     {
